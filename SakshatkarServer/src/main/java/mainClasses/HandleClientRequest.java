db54f2531784bcb0f63a7948a517003792b83aee
Shrey Pandey <shreypandey1509@gmail.com>
2019-09-16 00:04:36 +0530

    Fixed Logout Handler

--- SakshatkarServer/src/main/java/mainClasses/HandleClientRequest.java
+++ SakshatkarServer/src/main/java/mainClasses/HandleClientRequest.java
@@ -126,7 +126,8 @@ HandleClientRequest implements Runnable{
                     String userUID = ((LogoutRequest)request).getUserUID();
                     onlineUserHandler = new OnlineUserHandler(userUID);
                     onlineUserHandler.makeUserOffline();
-                    oos.writeObject(new Response(UIDGenerator.generateuid(),null,ResponseCode.SUCCESS));
+                    Response response = new Response(UIDGenerator.generateuid(),null,ResponseCode.SUCCESS);
+                    oos.writeObject(response);
                     oos.flush();
                 }
 
